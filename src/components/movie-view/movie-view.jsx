dceb335aad4c01e7e94becc60e7b08e8cee23f32
solzurch <151952240+solzurch@users.noreply.github.com>
2024-02-29 18:44:09 +0100

    import the PropTypes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
@@ -27,4 +29,14 @@ export const MovieView = ({ movie, onBackClick }) => {
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.array,
+    director: PropTypes.string,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
