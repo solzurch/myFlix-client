f025ad2a26d4f095691014c0b6b8a0716bb947fc
solzurch <151952240+solzurch@users.noreply.github.com>
2024-03-05 11:44:48 +0100

    upddate image path

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -26,7 +26,7 @@ export const MainView = () => {
             return {
               id: movie._id,
               title: movie.Title,
-              // image: movie.Image,
+              imagePath: movie.Image,
               genre: movie.Genre,
               description: movie.Description,
               director: movie.Director,
